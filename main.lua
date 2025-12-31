ae7a1bd00a2f55e2612def60e665996912d5642d
roberto freire <robertofreireferrazpassos@gmail.com>
2025-12-31 14:27:49 -0300

    clean up main file

--- main.lua
+++ main.lua
@@ -2,21 +2,12 @@ require("libs/luaes")
 
 -- DEMO --
 function _init()
-    FONT = love.graphics.getFont()
 end
-
-local pressed;
 
 function _update(dt)
     if btnp(4) or btnp(5) then
-        if not pressed then
-            sfx(1)
-        end
+        sfx(1)
     end
-end
-
-function love.mousereleased()
-    pressed = false
 end
 
 function _draw()
