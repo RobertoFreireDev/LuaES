4bbcaa5db90d5789647b8ad4f886dbab7e2a02d4
roberto freire <robertofreireferrazpassos@gmail.com>
2026-01-03 13:42:51 -0300

    add more tests

--- main.lua
+++ main.lua
@@ -82,6 +82,80 @@ local SFX6 = {
   {note="A4", volume = 1, length=6, wave=7, effect=0},
 }
 
+local SFX7 = {
+  {note="C7", volume = 1,   length=1, wave=6, effect=5},
+  {note="C7", volume = 0.8, length=1, wave=6, effect=5},
+  {note="C7", volume = 0.7, length=1, wave=6, effect=5},
+  {note="C7", volume = 0.6, length=1, wave=6, effect=5},
+  {note="C7", volume = 0.4, length=1, wave=6, effect=5},
+  {note="C7", volume = 0.4, length=1, wave=6, effect=5},
+  {note="G6", volume = 0.8, length=1, wave=6, effect=5},
+  {note="G6", volume = 0.6, length=1, wave=6, effect=5},
+}
+
+local SFX8 = { 
+  {note="C3", volume = 0.4,   length=1, wave=1, effect=9},
+  {note="C3", volume = 0.2, length=1, wave=1, effect=9},
+  {note="C3", volume = 0.4, length=1, wave=1, effect=9},
+  {note="C3", volume = 0.2, length=1, wave=1, effect=9},
+  {note="C3", volume = 0.4, length=1, wave=1, effect=9},
+  {note="C3", volume = 0.2, length=1, wave=1, effect=9},
+  {note="C3", volume = 0.1, length=1, wave=1, effect=9},
+  {note=0, volume = 0.0, length=1, wave=0, effect=9},
+}
+
+local SFX9 = {
+  {note="Ds2", volume = 0.2,   length=1, wave=7, effect=5},
+  {note="C5", volume = 0.1, length=1, wave=7, effect=5},
+  {note=0, volume = 0.0, length=1, wave=0, effect=5},
+  {note="Ds2", volume = 0.1, length=1, wave=7, effect=5},
+  {note="C5", volume = 0.2, length=1, wave=7, effect=5},
+  {note="Ds4", volume = 0.1, length=1, wave=7, effect=5},
+  {note="As4", volume = 0.2, length=1, wave=7, effect=5},
+  {note="Ds2", volume = 0.1, length=1, wave=7, effect=5},
+}
+
+local SFX10 = {
+  {note="Fs6", volume = 1,   length=1, wave=4, effect=0},
+  {note="B4", volume = 1, length=1, wave=5, effect=0},
+  {note="B5", volume = 1, length=1, wave=4, effect=0},
+  {note="F6", volume = 1, length=1, wave=5, effect=0},
+  {note="Fs5", volume = 1, length=1, wave=4, effect=0},
+  {note="B3", volume = 1, length=1, wave=5, effect=0},
+  {note="Ds5", volume = 1, length=1, wave=4, effect=0},
+  {note="G3", volume = 1, length=1, wave=5, effect=0},
+}
+
+local SFX11 = {
+  {note="E3", volume = 1, length=1, wave=3, effect=0},
+  {note="E3", volume = 1, length=1, wave=3, effect=0},
+  {note="E3", volume = 1, length=1, wave=3, effect=0},
+  {note="E3", volume = 1, length=1, wave=3, effect=0},
+  {note="E3", volume = 1, length=1, wave=2, effect=0},
+  {note="E3", volume = 1, length=1, wave=2, effect=0},
+  {note="E3", volume = 1, length=1, wave=2, effect=0},
+  {note="E3", volume = 1, length=1, wave=2, effect=0},
+}
+
+local SFX12 = {
+  {note="Fs2", volume = 0.2, length=1, wave=5, effect=0},
+  {note="Gs2", volume = 0.2, length=1, wave=5, effect=0},
+  {note="A2", volume = 0.2, length=1, wave=5, effect=0},
+  {note="B2", volume = 0.2, length=1, wave=5, effect=0},
+  {note="As4", volume = 0.2, length=1, wave=5, effect=0},
+  {note="Fs5", volume = 0.2, length=1, wave=5, effect=0},
+  {note="C7", volume = 1, length=1, wave=7, effect=0},
+  {note="B6", volume = 1, length=1, wave=7, effect=0},
+  {note="B6", volume = 1, length=1, wave=7, effect=0},
+  {note="A6", volume = 1, length=1, wave=7, effect=0},
+  {note="Fs6", volume = 1, length=1, wave=7, effect=0},
+  {note="D6", volume = 1, length=1, wave=7, effect=0},
+  {note="A5", volume = 1, length=1, wave=7, effect=0},
+  {note="E5", volume = 1, length=1, wave=7, effect=0},
+  {note="C5", volume = 1, length=1, wave=7, effect=0},
+  {note="A4", volume = 1, length=1, wave=7, effect=0},
+}
+
 indexsound = 0
 
 function addsounds(s)
@@ -98,6 +172,12 @@ function _init()
   addsounds(SFX4)
   addsounds(SFX5)
   addsounds(SFX6)
+  addsounds(SFX7)
+  addsounds(SFX8)
+  addsounds(SFX9)
+  addsounds(SFX10)
+  addsounds(SFX11)
+  addsounds(SFX12)
 end
 
 SOUNDINDEX = 5
@@ -108,6 +188,12 @@ SOUNDS = {
   { play = {4} },
   { play = {5} },
   { play = {6} },
+  { play = {7} },
+  { play = {8} },
+  { play = {9} },
+  { play = {10} },
+  { play = {11} },
+  { play = {12} },
 }
 
 delay = 1
