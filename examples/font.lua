55f0862b6bdec136c62bc5f00ce68d8d2daa180b
roberto freire <robertofreireferrazpassos@gmail.com>
2026-01-04 20:21:15 -0300

    improve font example

--- examples/font.lua
+++ examples/font.lua
@@ -2,6 +2,8 @@ require("libs/luaes")
 
 local idx = 1
 local limit = 6
+local modeidx = 1
+local modelimit = 4
 function _init()
     font(idx)
 end
@@ -14,13 +16,36 @@ function _update(dt)
         idx = idx - 1
         font(idx)
     end
+
+    if btnp(2) and modeidx < modelimit  then
+        modeidx = modeidx + 1
+    elseif btnp(3) and modeidx > 1 then
+        modeidx = modeidx - 1
+    end
+end
+
+local c = 1
+local sc = 26
+
+function printmode(t,x,y)
+    if modeidx == 2 or modeidx == 4 then
+        print(t,x+1,y,sc)
+    end
+    if modeidx == 3 or modeidx == 4 then
+        print(t,x,y+1,sc)
+    end
+    if modeidx == 4 then
+        print(t,x-1,y,sc)
+        print(t,x,y-1,sc)
+    end
+    print(t,x,y,c)
 end
 
 function _draw()
-    print("0123456789 {|}~", 0, 0, 4)
-    print("!\"#$%&'()*+,-./", 0, 20, 4)
-    print("ABCDEFGHIJKLMNO", 0, 40, 4)
-    print("PQRSTUVWXYZ", 0, 60, 4)
-    print("abcdefghijklmno", 0, 80, 4)
-    print("pqrstuvwxyz", 0, 100, 4)
+    printmode("0123456789 {|}~", 0, 0)
+    printmode("!\"#$%&'()*+,-./", 0, 20)
+    printmode("ABCDEFGHIJKLMNO", 0, 40)
+    printmode("PQRSTUVWXYZ", 0, 60)
+    printmode("abcdefghijklmno", 0, 80)
+    printmode("pqrstuvwxyz", 0, 100)
 end
